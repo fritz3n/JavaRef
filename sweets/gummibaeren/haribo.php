9389f5f75ddfeef72ee92d957475ac0994576d3d
Finn-Johannes Blumenstock <finn-blumenstock@web.de>
2019-12-18 09:11:42 +0100

    Changed shit

---
+++ sweets/gummibaeren/haribo.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Wool Street: Joints</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="/css/navbar.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="/css/style.css">
+        <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+    </head>
+    <body>
+        <?php include("../../../source/navbar.html") ?>
+        <div class="content">
+            <div class="item-container">
+                <div class="item-div">
+                    <img class="item-image" src="assets/haribo/haribo1.jpg" />
+                    <div class="item-title">Haribo 600g</div>
+                    <div class="item-description">
+                        <p>Feinste Haribo Gummibären</p>
+                        <p>Nettogewicht 600g</p>
+                    </div>
+                </div>
+                <div class="item-div">
+                    Test
+                </div>
+                <div class="item-div">
+                    Test
+                </div>
+                <div class="item-div">
+                    Test
+                </div>
+            </div>
+        </div>  
+    </body>
+</html>
